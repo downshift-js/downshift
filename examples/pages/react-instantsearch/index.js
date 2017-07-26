effe55caa446c0e19b8d7419bdac04a09929cb0c
Haroen Viaene <hello@haroen.me>
2017-07-26 10:00:33 +0200

    docs(algolia): add example for Algolia React InstantSearch
    This is using `connectAutoComplete` connector
    
    Maybe I misunderstood something while copy-pasting, but I'm not completely sure why only one result is visible.

--- examples/pages/react-instantsearch/index.js
+++ examples/pages/react-instantsearch/index.js
@@ -1,7 +1,49 @@
 import React from 'react'
+import {InstantSearch, Highlight} from 'react-instantsearch/dom'
+import {connectAutoComplete} from 'react-instantsearch/connectors'
+import Autocomplete from '../../other/react-autocompletely'
 
 export default Examples
 
+function RawAutoComplete({refine, hits}) {
+  return (
+    <Autocomplete onChange={item => alert(JSON.stringify(item))}>
+      <Autocomplete.Input onChange={e => refine(e.target.value)} />
+      <Autocomplete.Menu>
+        {({selectedItem, highlightedIndex}) =>
+          hits.map((item, index) =>
+            (<Autocomplete.Item
+              item={item}
+              index={index}
+              key={item.objectID}
+              style={{
+                backgroundColor: highlightedIndex === index ? 'gray' : 'white',
+                fontWeight: selectedItem === item ? 'bold' : 'normal',
+              }}
+            >
+              <Highlight attributeName="name" hit={item} tagName="mark" />
+            </Autocomplete.Item>),
+          )}
+      </Autocomplete.Menu>
+    </Autocomplete>
+  )
+}
+
+const AutoCompleteWithData = connectAutoComplete(RawAutoComplete)
+
 function Examples() {
-  return <div>react-instantsearch examples</div>
+  return (
+    <InstantSearch
+      appId="latency"
+      apiKey="6be0576ff61c053d5f9a3225e2a90f76"
+      indexName="actors"
+    >
+      Algolia{' '}
+      <a href="https://community.algolia.com/react-instantsearch/">
+        React InstantSearch
+      </a>{' '}
+      example
+      <AutoCompleteWithData />
+    </InstantSearch>
+  )
 }
