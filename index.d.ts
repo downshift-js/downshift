64e3d21eca206e88258f80b1b5274d22b9aae22e
Vu Tran <vu@vu-tran.com>
2017-07-30 09:00:02 -0700

    add TS definitions

---
+++ index.d.ts
@@ -0,0 +1,61 @@
+import * as React from 'react';
+
+declare namespace ReactAutocompletely {
+    interface AutocompleteProps {
+        children: React.ReactNode;
+        component?: any;
+        defaultSelectedIndex?: string;
+        defaultHighlightedIndex?: number;
+        getA11yStatusMessage?: Function;
+        getValue?: Function;
+        innerRef?: Function;
+        onChange: (item: any) => void;
+    }
+
+    interface ChildrenOptions {
+        highlightedIndex?: number;
+        setHighlightedIndex?: (index: number) => void;
+        inputValue?: string;
+        isOpen?: boolean;
+        toggleMenu?: (state: boolean) => void;
+        openMenu?: () => void;
+        closeMenu?: () => void;
+        selectedItem: any;
+        clearSelection?: () => void;
+        selectItem?: (item: any) => void;
+        selectItemAtIndex?: (index: number) => void;
+        selectHighlightedIndex?: () => void;
+    }
+
+    type ChildrenFunction = (options: ChildrenOptions) => void;
+
+    interface InputProps {
+        defaultValue?: string;
+    }
+    class Input extends React.Component<InputProps, {}> {}
+
+    interface ControllerProps {
+        children: ChildrenFunction;
+    }
+    class Controller extends React.Component<ControllerProps, {}> {}
+
+    interface ButtonProps {}
+    class Button extends React.Component<ButtonProps, {}> {}
+
+    interface ItemProps {
+        index: number;
+        value: any;
+    }
+    class Item extends React.Component<ItemProps, {}> {}
+    class Autocomplete extends React.Component<
+        ReactAutocompletely.AutocompleteProps,
+        {}
+    > {
+        public static Button: typeof ReactAutocompletely.Button;
+        public static Controller: typeof ReactAutocompletely.Controller;
+        public static Input: typeof ReactAutocompletely.Input;
+        public static Item: typeof ReactAutocompletely.Item;
+    }
+}
+
+export default ReactAutocompletely.Autocomplete;
