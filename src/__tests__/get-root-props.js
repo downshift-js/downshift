e2e2bf7bf9f209b07f0fc8863682ce35965524b9
Kent C. Dodds <kent+github@doddsfamily.us>
2017-08-07 16:07:11 -0600

    test: add tests for the render method (#106)

---
+++ src/__tests__/get-root-props.js
@@ -0,0 +1,72 @@
+import React from 'react'
+import {mount} from 'enzyme'
+import Downshift from '../'
+
+const MyDiv = ({innerRef, ...rest}) => <div ref={innerRef} {...rest} />
+
+test('no children provided renders nothing', () => {
+  const MyComponent = () => <Downshift />
+  expect(mount(<MyComponent />).html()).toBe(null)
+})
+
+test('returning null renders nothing', () => {
+  const MyComponent = () =>
+    (<Downshift>
+      {() => null}
+    </Downshift>)
+  expect(mount(<MyComponent />).html()).toBe(null)
+})
+
+test('returning a composite component without calling getRootProps results in an error', () => {
+  const MyComponent = () =>
+    (<Downshift>
+      {() => <MyDiv />}
+    </Downshift>)
+  expect(() => mount(<MyComponent />)).toThrowErrorMatchingSnapshot()
+})
+
+test('returning a composite component and calling getRootProps without a refKey results in an error', () => {
+  const MyComponent = () =>
+    (<Downshift>
+      {({getRootProps}) => <MyDiv {...getRootProps()} />}
+    </Downshift>)
+  expect(() => mount(<MyComponent />)).toThrowErrorMatchingSnapshot()
+})
+
+test('returning a DOM element and calling getRootProps with a refKey results in an error', () => {
+  const MyComponent = () =>
+    (<Downshift>
+      {({getRootProps}) => <div {...getRootProps({refKey: 'blah'})} />}
+    </Downshift>)
+  expect(() => mount(<MyComponent />)).toThrowErrorMatchingSnapshot()
+})
+
+test('not applying the ref prop results in an error', () => {
+  const MyComponent = () =>
+    (<Downshift>
+      {({getRootProps}) => {
+        const {onClick} = getRootProps()
+        return <div onClick={onClick} />
+      }}
+    </Downshift>)
+  expect(() => mount(<MyComponent />)).toThrowErrorMatchingSnapshot()
+})
+
+test('not applying the onClick prop results in an error', () => {
+  const MyComponent = () =>
+    (<Downshift>
+      {({getRootProps}) => {
+        const {ref} = getRootProps()
+        return <div ref={ref} />
+      }}
+    </Downshift>)
+  expect(() => mount(<MyComponent />)).toThrowErrorMatchingSnapshot()
+})
+
+test('renders fine when rendering a composite component and applying getRootProps properly', () => {
+  const MyComponent = () =>
+    (<Downshift>
+      {({getRootProps}) => <MyDiv {...getRootProps({refKey: 'innerRef'})} />}
+    </Downshift>)
+  expect(() => mount(<MyComponent />)).not.toThrow()
+})
