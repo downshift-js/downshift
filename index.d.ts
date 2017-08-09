ec02f85e6f9231ec710bb92886d2ff35dc80f90b
Vu Tran <vutran@dropbox.com>
2017-08-08 17:12:14 -0700

    update with new API changes

--- index.d.ts
+++ index.d.ts
@@ -1,61 +1,105 @@
-import * as React from 'react';
-
 declare namespace Downshift {
-    interface AutocompleteProps {
-        children: React.ReactNode;
-        component?: any;
-        defaultSelectedIndex?: string;
-        defaultHighlightedIndex?: number;
-        getA11yStatusMessage?: Function;
-        getValue?: Function;
-        innerRef?: Function;
-        onChange: (item: any) => void;
-    }
-
-    interface ChildrenOptions {
-        highlightedIndex?: number;
-        setHighlightedIndex?: (index: number) => void;
-        inputValue?: string;
-        isOpen?: boolean;
-        toggleMenu?: (state: boolean) => void;
-        openMenu?: () => void;
-        closeMenu?: () => void;
-        selectedItem: any;
-        clearSelection?: () => void;
-        selectItem?: (item: any) => void;
-        selectItemAtIndex?: (index: number) => void;
-        selectHighlightedIndex?: () => void;
-    }
-
-    type ChildrenFunction = (options: ChildrenOptions) => void;
-
-    interface InputProps {
-        defaultValue?: string;
-    }
-    class Input extends React.Component<InputProps, {}> {}
-
-    interface ControllerProps {
+    interface Props {
         children: ChildrenFunction;
+        defaultHighlightedIndex?: number;
+        defaultSelectedItem?: any;
+        defaultInputValue?: string;
+        defaultIsOpen?: boolean;
+        getA11yStatusMessage?: (options: A11StatusMessageOptions) => any;
+        itemToString?: (item: any) => string;
+        onChange?: (options: ChangeOptions) => void;
+        onStateChange?: (options: StateChangeOptions) => void;
+        onClick?: Function;
+        selectedItem?: any;
+        isOpen?: boolean;
+        inputValue?: string;
+        highlightedIndex?: number;
     }
-    class Controller extends React.Component<ControllerProps, {}> {}
 
-    interface ButtonProps {}
-    class Button extends React.Component<ButtonProps, {}> {}
+    interface A11StatusMessageOptions {
+        highlightedIndex: number;
+        highlightedValue: any;
+        inputValue: string;
+        isOpen: boolean;
+        itemToString: (item: any) => string;
+        previousResultCount: number;
+        resultCount: number;
+        selectedItem: any;
+    }
 
-    interface ItemProps {
+    interface ChangeOptions {
+        selectedItem: any;
+        previousItem: any;
+    }
+
+    interface StateChangeOptions {
+        highlightedIndex: number;
+        inputValue: string;
+        isOpen: boolean;
+        selectedItem: any;
+    }
+
+    interface GetRootPropsOptions {
+        refKey: string;
+    }
+
+    interface GetInputPropsOptions extends React.HTMLProps<HTMLInputElement> { }
+
+    interface GetLabelPropsOptions extends React.HTMLProps<HTMLLabelElement> { }
+
+    interface GetButtonPropsOptions extends React.HTMLProps<HTMLButtonElement> {
+        // actions
+        clearSelection: () => void;
+        closeMenu: () => void;
+        openMenu: () => void;
+        selectHighlightedItem: () => void;
+        selectItem: (item: any) => void;
+        selectItemAtIndex: (index: number) => void;
+        setHighlightedIndex: (index: number) => void;
+        toggleMenu: (state: boolean) => void;
+
+        // state
+        highlightedIndex: number;
+        inputValue: string;
+        isOpen: boolean;
+        selectedItem: any;
+    }
+
+    interface GetItemPropsOptions {
         index: number;
-        value: any;
+        item: any;
     }
-    class Item extends React.Component<ItemProps, {}> {}
-    class Autocomplete extends React.Component<
-        Downshift.AutocompleteProps,
-        {}
-    > {
-        public static Button: typeof Downshift.Button;
-        public static Controller: typeof Downshift.Controller;
-        public static Input: typeof Downshift.Input;
-        public static Item: typeof Downshift.Item;
+
+    interface ControllerStateAndHelpers {
+        // prop getters
+        getRootProps: (options: GetRootPropsOptions) => any;
+        getButtonProps: (options: GetButtonPropsOptions) => any;
+        getLabelProps: (options: GetLabelPropsOptions) => any;
+        getInputProps: (options: GetInputPropsOptions) => any;
+        getItemProps: (options: GetItemPropsOptions) => any;
+
+        // actions
+        openMenu: () => void;
+        closeMenu: () => void;
+        toggleMenu: () => void;
+        selectItem: (item: any) => void;
+        selectItemAtIndex: (index: number) => void;
+        selectHighlightedItem: (index: number) => void;
+        setHighlightedItem: (index: number) => void;
+        clearSelection: () => void;
+
+        // state
+        highlightedIndex: number;
+        inputValue: string;
+        isOpen: boolean;
+        selectedItem: any;
     }
+
+    type ChildrenFunction = (options: ControllerStateAndHelpers) => React.ReactNode;
+    interface Downshift extends React.ComponentClass<Downshift.Props> { }
 }
 
-export default Downshift.Autocomplete;
+declare module 'downshift' {
+    const Downshift: Downshift.Downshift;
+    export default Downshift;
+}
