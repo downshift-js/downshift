e74a6d636da9b9a8a0ecad5dfdcfa1c1280df7bc
Vu Tran <vu@vu-tran.com>
2017-08-20 05:59:55 -0700

    fix: update entry file for ts consumption (#144)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 import Downshift from './downshift'
 
+/*
+ * Fix importing in typescript after rollup compilation
+ * https://github.com/rollup/rollup/issues/1156
+ * https://github.com/Microsoft/TypeScript/issues/13017#issuecomment-268657860
+ */
+Downshift.default = Downshift
+
 export default Downshift
