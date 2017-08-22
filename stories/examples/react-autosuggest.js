97a80da60c263c9602efa67eec133853ee627ce7
Kent C. Dodds <kent+github@doddsfamily.us>
2017-08-22 12:12:31 -0600

    feat(stateChangeTypes): add a state change type to everything (#147)
    This is an undocumented API, but I think it'll be really useful for some
    use cases so we'll try to support it.
    
    I also kinda broke a few APIs in the process. Shouldn't impact anyone,
    but it is technically different. Please see the changes in the README.

---
+++ stories/examples/react-autosuggest.js
@@ -0,0 +1,140 @@
+import React, {Component} from 'react'
+import matchSorter from 'match-sorter'
+import Downshift from '../../src'
+
+class Examples extends Component {
+  items = ['Black', 'Red', 'Green', 'Blue', 'Orange', 'Purple']
+  state = {
+    selectedColor: '',
+    inputValue: '',
+    itemsToShow: this.items,
+  }
+  userInputtedValue = ''
+
+  changeHandler = selectedColor => {
+    this.setState({selectedColor})
+  }
+
+  onStateChange = changes => {
+    let {selectedColor, inputValue, itemsToShow} = this.state
+    const isClosingMenu = changes.hasOwnProperty('isOpen') && !changes.isOpen
+    if (
+      changes.type === Downshift.stateChangeTypes.keyDownEscape &&
+      !isClosingMenu
+    ) {
+      selectedColor = null
+    }
+    if (changes.hasOwnProperty('inputValue')) {
+      if (changes.type === Downshift.stateChangeTypes.keyDownEscape) {
+        inputValue = this.userInputtedValue
+      } else {
+        inputValue = changes.inputValue
+        this.userInputtedValue = changes.inputValue
+      }
+    }
+    itemsToShow = this.userInputtedValue
+      ? matchSorter(this.items, this.userInputtedValue)
+      : this.items
+    if (
+      changes.hasOwnProperty('highlightedIndex') &&
+      (changes.type === Downshift.stateChangeTypes.keyDownArrowUp ||
+        changes.type === Downshift.stateChangeTypes.keyDownArrowDown)
+    ) {
+      inputValue = itemsToShow[changes.highlightedIndex]
+    }
+    if (isClosingMenu) {
+      inputValue = selectedColor
+      this.userInputtedValue = selectedColor
+    }
+    this.setState({inputValue, itemsToShow, selectedColor})
+  }
+
+  render() {
+    const {inputValue, selectedColor, itemsToShow} = this.state
+    return (
+      <div>
+        <div
+          style={{
+            margin: '50px auto',
+            maxWidth: 600,
+            textAlign: 'center',
+          }}
+        >
+          <h2>auto-fill on keydown</h2>
+          <div style={{display: 'flex', justifyContent: 'center'}}>
+            <span
+              style={{
+                height: '2em',
+                width: '2em',
+                padding: '.3em',
+                borderRadius: '5px',
+                marginRight: '.5em',
+                backgroundColor: selectedColor ? selectedColor : 'transparent',
+              }}
+            />
+            <BasicAutocomplete
+              items={itemsToShow}
+              selectedItem={selectedColor}
+              inputValue={inputValue}
+              onChange={this.changeHandler}
+              onStateChange={this.onStateChange}
+            />
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+function BasicAutocomplete({
+  items,
+  inputValue,
+  selectedItem,
+  onChange,
+  onStateChange,
+}) {
+  return (
+    <Downshift
+      inputValue={inputValue}
+      selectedItem={selectedItem}
+      onChange={onChange}
+      onStateChange={onStateChange}
+    >
+      {({
+        getInputProps,
+        getItemProps,
+        highlightedIndex,
+        isOpen,
+        selectedItem,
+      }) =>
+        (<div>
+          <input {...getInputProps({placeholder: 'Enter color here'})} />
+          {isOpen &&
+            <div
+              style={{
+                maxHeight: 200,
+                overflowY: 'scroll',
+              }}
+            >
+              {items.map((item, index) =>
+                (<div
+                  key={item}
+                  {...getItemProps({
+                    item,
+                    index,
+                    style: {
+                      backgroundColor:
+                        highlightedIndex === index ? 'gray' : 'white',
+                      fontWeight: selectedItem === item ? 'bold' : 'normal',
+                    },
+                  })}
+                >
+                  {item}
+                </div>),
+              )}
+            </div>}
+        </div>)}
+    </Downshift>
+  )
+}
+export default Examples
