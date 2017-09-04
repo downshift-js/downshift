0f71611632bd728c02cd62ccd865e80b02f04473
Frank Tan <tansongyang@gmail.com>
2017-09-03 21:06:05 -0400

    feat: improve TypeScript typings (#165)
    * Update actions types
    
    * Remove props from GetButtonPropsOptions
    
    These props are not required for getButtonProps.
    
    * Make getItemProps index optional
    
    * Update state types
    
    * Update DownshiftProps and A11StatusMessageOptions
    
    Allow null in places where the docs say they can go. Add missing `itemCount` property.
    
    * Remove version number from types
    
    * Update contributors
    
    * Address PR comments
    
    * Rename `changes` back to `options`.
    * Add optional arguments in the
    right places.
    * Use `Function` as type for `cb`.
    * Use `object` ast type
    for `otherStateToSet`.
    * Fix incorrect name `setHighlightedItem` by
    changing it to
    `setHighlightedIndex`.
    
    * Address updated PR comments
    
    * Rename `A11StatusMessageOptions` to `A11yStatusMessageOptions`.
    * Remove `ChangeOptions`.
    * Remove `onClick` prop.
    
    * Update README.md

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -2,24 +2,24 @@ import * as React from 'react';
 
 export interface DownshiftProps {
     children: ChildrenFunction;
-    defaultHighlightedIndex?: number;
+    defaultHighlightedIndex?: number | null;
     defaultSelectedItem?: any;
     defaultInputValue?: string;
     defaultIsOpen?: boolean;
-    getA11yStatusMessage?: (options: A11StatusMessageOptions) => any;
+    getA11yStatusMessage?: (options: A11yStatusMessageOptions) => any;
     itemToString?: (item: any) => string;
     onChange?: (selectedItem: any, stateAndHelpers: ControllerStateAndHelpers) => void;
     onStateChange?: (options: StateChangeOptions, stateAndHelpers: ControllerStateAndHelpers) => void;
     onUserAction?: (options: StateChangeOptions, stateAndHelpers: ControllerStateAndHelpers) => void;
-    onClick?: Function;
+    itemCount?: number;
     selectedItem?: any;
     isOpen?: boolean;
     inputValue?: string;
     highlightedIndex?: number;
 }
 
-export interface A11StatusMessageOptions {
-    highlightedIndex: number;
+export interface A11yStatusMessageOptions {
+    highlightedIndex: number | null;
     highlightedValue: any;
     inputValue: string;
     isOpen: boolean;
@@ -27,11 +27,6 @@ export interface A11StatusMessageOptions {
     previousResultCount: number;
     resultCount: number;
     selectedItem: any;
-}
-
-export interface ChangeOptions {
-    selectedItem: any;
-    previousItem: any;
 }
 
 export interface StateChangeOptions {
@@ -49,30 +44,14 @@ export interface GetInputPropsOptions extends React.HTMLProps<HTMLInputElement> 
 
 export interface GetLabelPropsOptions extends React.HTMLProps<HTMLLabelElement> { }
 
-export interface GetButtonPropsOptions extends React.HTMLProps<HTMLButtonElement> {
-    // actions
-    clearSelection: () => void;
-    closeMenu: () => void;
-    openMenu: () => void;
-    selectHighlightedItem: () => void;
-    selectItem: (item: any) => void;
-    selectItemAtIndex: (index: number) => void;
-    setHighlightedIndex: (index: number) => void;
-    toggleMenu: (state: boolean) => void;
-
-    // state
-    highlightedIndex: number;
-    inputValue: string;
-    isOpen: boolean;
-    selectedItem: any;
-}
+export interface GetButtonPropsOptions extends React.HTMLProps<HTMLButtonElement> { }
 
 interface OptionalExtraGetItemPropsOptions {
     [key: string]: any;
 }
 
 export interface GetItemPropsOptions extends OptionalExtraGetItemPropsOptions {
-    index: number;
+    index?: number;
     item: any;
 }
 
@@ -85,18 +64,20 @@ export interface ControllerStateAndHelpers {
     getItemProps: (options: GetItemPropsOptions) => any;
 
     // actions
-    openMenu: () => void;
-    closeMenu: () => void;
-    toggleMenu: () => void;
-    selectItem: (item: any) => void;
-    selectItemAtIndex: (index: number) => void;
-    selectHighlightedItem: (index: number) => void;
-    setHighlightedItem: (index: number) => void;
-    clearSelection: () => void;
+    openMenu: (cb?: Function) => void;
+    closeMenu: (cb?: Function) => void;
+    toggleMenu: (cb?: Function) => void;
+    selectItem: (item: any, otherStateToSet?: object, cb?: Function) => void;
+    selectItemAtIndex: (index: number, otherStateToSet?: object, cb?: Function) => void;
+    selectHighlightedItem: (otherStateToSet?: object, cb?: Function) => void;
+    setHighlightedIndex: (index: number, otherStateToSet?: object, cb?: Function) => void;
+    clearSelection: (cb?: Function) => void;
+    reset: (otherStateToSet?: object, cb?: Function) => void;
+    itemToString: (item: any) => void;
 
     // state
-    highlightedIndex: number;
-    inputValue: string;
+    highlightedIndex: number | null;
+    inputValue: string | null;
     isOpen: boolean;
     selectedItem: any;
 }
