d6515beb82e341b72df1738f08736bd8414d10cf
yp <yp@users.noreply.github.com>
2017-11-09 17:15:38 +0100

    feat: add the option `suppressRefError` to `getRootProps`. (#241)
    * Add the option `suppressRefError` to `getRootProps`.
    
    Fixes paypal/downshift#235.
    
    * Add myself as contributor.
    
    * Fix Markdown in README.
    
    * Add also 'test' as contribution.

--- src/__tests__/downshift.get-root-props.js
+++ src/__tests__/downshift.get-root-props.js
@@ -66,4 +66,51 @@ test('renders fine when rendering a composite component and applying getRootProp
   expect(() => mount(<MyComponent />)).not.toThrow()
 })
 
+test('returning a composite component and calling getRootProps without a refKey does not result in an error if suppressRefError is true', () => {
+  const MyComponent = () => (
+    <Downshift>
+      {({getRootProps}) => (
+        <MyDiv {...getRootProps({}, {suppressRefError: true})} />
+      )}
+    </Downshift>
+  )
+  expect(() => mount(<MyComponent />)).not.toThrow()
+})
+
+test('returning a DOM element and calling getRootProps with a refKey does not result in an error if suppressRefError is true', () => {
+  const MyComponent = () => (
+    <Downshift>
+      {({getRootProps}) => (
+        <div {...getRootProps({refKey: 'blah'}, {suppressRefError: true})} />
+      )}
+    </Downshift>
+  )
+  expect(() => mount(<MyComponent />)).not.toThrow()
+})
+
+test('not applying the ref prop results in an error does not result in an error if suppressRefError is true', () => {
+  const MyComponent = () => (
+    <Downshift>
+      {({getRootProps}) => {
+        const {onClick} = getRootProps({}, {suppressRefError: true})
+        return <div onClick={onClick} />
+      }}
+    </Downshift>
+  )
+  expect(() => mount(<MyComponent />)).not.toThrow()
+})
+
+test('renders fine when rendering a composite component and applying getRootProps properly even if suppressRefError is true', () => {
+  const MyComponent = () => (
+    <Downshift>
+      {({getRootProps}) => (
+        <MyDiv
+          {...getRootProps({refKey: 'innerRef'}, {suppressRefError: true})}
+        />
+      )}
+    </Downshift>
+  )
+  expect(() => mount(<MyComponent />)).not.toThrow()
+})
+
 /* eslint no-console:0 */
--- src/downshift.js
+++ src/downshift.js
@@ -426,11 +426,15 @@ class Downshift extends Component {
 
   rootRef = node => (this._rootNode = node)
 
-  getRootProps = ({refKey = 'ref', ...rest} = {}) => {
+  getRootProps = (
+    {refKey = 'ref', ...rest} = {},
+    {suppressRefError = false} = {},
+  ) => {
     // this is used in the render to know whether the user has called getRootProps.
     // It uses that to know whether to apply the props automatically
     this.getRootProps.called = true
     this.getRootProps.refKey = refKey
+    this.getRootProps.suppressRefError = suppressRefError
     return {
       [refKey]: this.rootRef,
       ...rest,
@@ -772,6 +776,7 @@ class Downshift extends Component {
     // apply the props for them.
     this.getRootProps.called = false
     this.getRootProps.refKey = undefined
+    this.getRootProps.suppressRefError = undefined
     // we do something similar for getLabelProps
     this.getLabelProps.called = false
     // and something similar for getInputProps
@@ -781,7 +786,9 @@ class Downshift extends Component {
       return null
     }
     if (this.getRootProps.called) {
-      validateGetRootPropsCalledCorrectly(element, this.getRootProps)
+      if (!this.getRootProps.suppressRefError) {
+        validateGetRootPropsCalledCorrectly(element, this.getRootProps)
+      }
       return element
     } else if (isDOMElement(element)) {
       // they didn't apply the root props, but we can clone
