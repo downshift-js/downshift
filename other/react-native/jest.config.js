c2d4c95061189bc4de94e4463010ba1bfa64daff
Eli Perkins <eli@clubhouse.io>
2018-02-08 10:59:05 -0500

    Add React Native test to test:build command
    This leverages Jest projects to use both different configs for misc-tests and react-native

--- other/react-native/jest.config.js
+++ other/react-native/jest.config.js
@@ -4,6 +4,7 @@ module.exports = Object.assign(jestConfig, {
   preset: 'react-native',
   testEnvironment: 'node',
   transformIgnorePatterns: ['<rootDir>/node_modules/(?!react-native)/'],
+  rootDir: '../../',
   roots: ['.'],
   testMatch: ['<rootDir>/other/react-native/__tests__/**/*.js?(x)'],
 })
