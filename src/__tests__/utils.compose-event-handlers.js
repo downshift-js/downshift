2ba151e2baf6ab55c32ad110801d0f4799249005
Eric Edem <ericedem@gmail.com>
2018-03-05 13:06:37 -0800

    refactor(composeEventHandlers): use jest for testing

--- src/__tests__/utils.compose-event-handlers.js
+++ src/__tests__/utils.compose-event-handlers.js
@@ -1,45 +1,38 @@
 import {composeEventHandlers} from '../utils'
 
 test('prevent default handlers when defaultPrevented is true', () => {
-  const handler = composeEventHandlers(
-    e => {
-      e.defaultPrevented = true
-    },
-    () => {
-      throw new Error(
-        'preventing default should prevent default downshift behavior',
-      )
-    },
-  )
-  handler({})
+  const customHandler = jest.fn(e => {
+    e.defaultPrevented = true
+  })
+  const defaultHandler = jest.fn(() => {})
+
+  const composedHandler = composeEventHandlers(customHandler, defaultHandler)
+
+  composedHandler({})
+  expect(customHandler).toHaveBeenCalledTimes(1)
+  expect(defaultHandler).toHaveBeenCalledTimes(0)
 })
 
 test('prevent default handlers when defaultDownshiftPrevented is true', () => {
-  const handler = composeEventHandlers(
-    e => {
-      e.preventDownshiftDefault = true
-    },
-    () => {
-      throw new Error(
-        'setting defaultDownshiftPrevented = true should prevent default downshift behavior',
-      )
-    },
-  )
-  handler({})
+  const customHandler = jest.fn(e => {
+    e.preventDownshiftDefault = true
+  })
+  const defaultHandler = jest.fn(() => {})
+
+  const composedHandler = composeEventHandlers(customHandler, defaultHandler)
+
+  composedHandler({})
+  expect(customHandler).toHaveBeenCalledTimes(1)
+  expect(defaultHandler).toHaveBeenCalledTimes(0)
 })
 
 test('call default handler when defaultDownshiftPrevented and defaultPrevented are false', () => {
-  let defaultCalled = false
+  const customHandler = jest.fn(() => {})
+  const defaultHandler = jest.fn(() => {})
 
-  const handler = composeEventHandlers(
-    () => {},
-    () => {
-      defaultCalled = true
-    },
-  )
-  handler({})
+  const composedHandler = composeEventHandlers(customHandler, defaultHandler)
 
-  if (!defaultCalled) {
-    throw new Error("default handler not called when default wasn't prevented")
-  }
+  composedHandler({})
+  expect(customHandler).toHaveBeenCalledTimes(1)
+  expect(defaultHandler).toHaveBeenCalledTimes(1)
 })
