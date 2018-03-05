1366d0a2c50485defbf306591c91727e297ced08
Eric Edem <ericedem@gmail.com>
2018-03-05 14:26:00 -0800

    refactor(composeEventHandler): use parameter-less jest.fn()

--- src/__tests__/utils.compose-event-handlers.js
+++ src/__tests__/utils.compose-event-handlers.js
@@ -4,7 +4,7 @@ test('prevent default handlers when defaultPrevented is true', () => {
   const customHandler = jest.fn(e => {
     e.defaultPrevented = true
   })
-  const defaultHandler = jest.fn(() => {})
+  const defaultHandler = jest.fn()
 
   const composedHandler = composeEventHandlers(customHandler, defaultHandler)
 
@@ -17,7 +17,7 @@ test('prevent default handlers when defaultDownshiftPrevented is true', () => {
   const customHandler = jest.fn(e => {
     e.preventDownshiftDefault = true
   })
-  const defaultHandler = jest.fn(() => {})
+  const defaultHandler = jest.fn()
 
   const composedHandler = composeEventHandlers(customHandler, defaultHandler)
 
@@ -27,8 +27,8 @@ test('prevent default handlers when defaultDownshiftPrevented is true', () => {
 })
 
 test('call default handler when defaultDownshiftPrevented and defaultPrevented are false', () => {
-  const customHandler = jest.fn(() => {})
-  const defaultHandler = jest.fn(() => {})
+  const customHandler = jest.fn()
+  const defaultHandler = jest.fn()
 
   const composedHandler = composeEventHandlers(customHandler, defaultHandler)
 
