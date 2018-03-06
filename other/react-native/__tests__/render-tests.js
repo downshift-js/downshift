21baedb254482860ce2846f6ffde64a59e74422e
Eli Perkins <eli@clubhouse.io>
2018-03-06 16:57:53 -0500

    Remove invalid test for React Native
    This test doesn't assert the intended behavior, which is composing onChange and onChangeText

--- other/react-native/__tests__/render-tests.js
+++ other/react-native/__tests__/render-tests.js
@@ -49,38 +49,6 @@ test('can use children instead of render prop', () => {
   expect(renderSpy).toHaveBeenCalledTimes(1)
 })
 
-test('getInputProps composes onChange with onChangeText', () => {
-  const onChange = jest.fn()
-  const onInput = jest.fn()
-
-  const RootView = ({innerRef, ...rest}) => <View ref={innerRef} {...rest} />
-  const Input = jest.fn(props => <TextInput {...props} />)
-  const renderSpy = jest.fn(({getRootProps, getInputProps, getItemProps}) => (
-    <RootView {...getRootProps({refKey: 'innerRef'})}>
-      <Input {...getInputProps({onChange, onInput})} />
-      <View>
-        <Text {...getItemProps({item: 'foo', index: 0})}>foo</Text>
-        <Text {...getItemProps({item: 'bar', index: 1})}>bar</Text>
-      </View>
-    </RootView>
-  ))
-  const element = <Downshift render={renderSpy} />
-  TestRenderer.create(element)
-
-  expect(Input).toHaveBeenCalledTimes(1)
-  const [[firstArg]] = Input.mock.calls
-  expect(firstArg).toMatchObject({
-    onChangeText: expect.any(Function),
-  })
-  expect(firstArg.onChange).toBeUndefined()
-  const fakeEvent = 'foobar'
-  firstArg.onChangeText(fakeEvent)
-  expect(onChange).toHaveBeenCalledTimes(1)
-  expect(onChange).toHaveBeenCalledWith(fakeEvent)
-  expect(onInput).toHaveBeenCalledTimes(1)
-  expect(onInput).toHaveBeenCalledWith(fakeEvent)
-})
-
 /*
  eslint
   react/prop-types: 0,
