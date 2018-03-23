7f66c4e143a9e0b5155d973469d3c1f953be4f87
Kent C. Dodds <kent@doddsfamily.us>
2018-03-23 15:12:09 -0600

    fix(aria): improve WAI-ARIA compliance
    Big thanks to @1Copenut for leaving feedback in
    https://github.com/paypal/downshift/pull/285

--- src/__tests__/downshift.aria.js
+++ src/__tests__/downshift.aria.js
@@ -24,34 +24,43 @@ test('can override the ids', () => {
   expect(container.firstChild).toMatchSnapshot()
 })
 
-function defaultRenderFn({
-  getInputProps,
-  getToggleButtonProps,
-  getLabelProps,
-  getMenuProps,
-  getItemProps,
-}) {
-  return (
-    <div>
-      <label data-testid="label" {...getLabelProps()}>
-        label
-      </label>
-      <input data-testid="input" {...getInputProps()} />
-      <button data-testid="button" {...getToggleButtonProps()} />
-      <ul data-testid="menu" {...getMenuProps()}>
-        <li data-testid="item-0" {...getItemProps({item: 'item', index: 0})}>
-          item
-        </li>
-      </ul>
-    </div>
-  )
-}
+test('if aria-label is provided to the menu then aria-labelledby is not applied to the label', () => {
+  const customLabel = 'custom menu label'
+  const {menu} = renderDownshift({
+    menuProps: {'aria-label': customLabel},
+  })
+  expect(menu.getAttribute('aria-labelledby')).toBeNull()
+  expect(menu.getAttribute('aria-label')).toBe(customLabel)
+})
 
-function renderDownshift({renderFn = defaultRenderFn, props} = {}) {
+function renderDownshift({renderFn, props, menuProps} = {}) {
+  function defaultRenderFn({
+    getInputProps,
+    getToggleButtonProps,
+    getLabelProps,
+    getMenuProps,
+    getItemProps,
+  }) {
+    return (
+      <div>
+        <label data-testid="label" {...getLabelProps()}>
+          label
+        </label>
+        <input data-testid="input" {...getInputProps()} />
+        <button data-testid="button" {...getToggleButtonProps()} />
+        <ul data-testid="menu" {...getMenuProps(menuProps)}>
+          <li data-testid="item-0" {...getItemProps({item: 'item', index: 0})}>
+            item
+          </li>
+        </ul>
+      </div>
+    )
+  }
+
   let renderArg
   const renderSpy = jest.fn(controllerArg => {
     renderArg = controllerArg
-    return renderFn(controllerArg)
+    return renderFn || defaultRenderFn(controllerArg)
   })
   const utils = renderToDOM(<Downshift {...props} render={renderSpy} />)
   return {
