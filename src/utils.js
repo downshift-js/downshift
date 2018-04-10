6892d53d32f6b8a3d16f65d038ea304d9979f9f2
Dale Hurwitz <dale.hurwitz@qantasloyalty.com.au>
2018-04-10 14:34:09 +1000

    Fix: replace String method not supported in IE11

--- src/utils.js
+++ src/utils.js
@@ -292,7 +292,7 @@ function pickState(state = {}) {
 function normalizeArrowKey(event) {
   const { key, keyCode } = event
   /* istanbul ignore next (ie) */
-  if (keyCode >= 37 && keyCode <= 40 && !key.startsWith('Arrow')) {
+  if (keyCode >= 37 && keyCode <= 40 && key.indexOf('Arrow') !== 0) {
     return `Arrow${key}`
   }
   return key
