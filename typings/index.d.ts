49c36168faec7773ff43a3d8d3edca2ba4abc06f
dovidweisz <dave@dweisz.com>
2018-04-17 10:01:39 -0400

    let prettier run

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -27,7 +27,7 @@ export enum StateChangeTypes {
 }
 
 export interface BreakingChangesProps {
-  resetInputOnSelection?: boolean;
+  resetInputOnSelection?: boolean
 }
 
 export interface DownshiftProps<Item> {
@@ -72,7 +72,7 @@ export interface DownshiftProps<Item> {
     options: StateChangeOptions<Item>,
     stateAndHelpers: ControllerStateAndHelpers<Item>,
   ) => void
-  breakingChanges?: BreakingChangesProps;
+  breakingChanges?: BreakingChangesProps
 }
 
 export interface Environment {
@@ -91,7 +91,7 @@ export interface A11yStatusMessageOptions<Item> {
   selectedItem: Item
 }
 
-export interface StateChangeOptions<Item> extends DownshiftState<Item>  {
+export interface StateChangeOptions<Item> extends DownshiftState<Item> {
   type: StateChangeTypes
 }
 
@@ -133,17 +133,30 @@ export interface PropGetters<Item> {
 }
 
 export interface Actions<Item> {
-  reset: (otherStateToSet?: Partial<StateChangeOptions<Item>>, cb?: Callback) => void
+  reset: (
+    otherStateToSet?: Partial<StateChangeOptions<Item>>,
+    cb?: Callback,
+  ) => void
   openMenu: (cb?: Callback) => void
   closeMenu: (cb?: Callback) => void
-  toggleMenu: (otherStateToSet?: Partial<StateChangeOptions<Item>>, cb?: Callback) => void
-  selectItem: (item: Item, otherStateToSet?: Partial<StateChangeOptions<Item>>, cb?: Callback) => void
+  toggleMenu: (
+    otherStateToSet?: Partial<StateChangeOptions<Item>>,
+    cb?: Callback,
+  ) => void
+  selectItem: (
+    item: Item,
+    otherStateToSet?: Partial<StateChangeOptions<Item>>,
+    cb?: Callback,
+  ) => void
   selectItemAtIndex: (
     index: number,
     otherStateToSet?: Partial<StateChangeOptions<Item>>,
     cb?: Callback,
   ) => void
-  selectHighlightedItem: (otherStateToSet?: Partial<StateChangeOptions<Item>>, cb?: Callback) => void
+  selectHighlightedItem: (
+    otherStateToSet?: Partial<StateChangeOptions<Item>>,
+    cb?: Callback,
+  ) => void
   setHighlightedIndex: (
     index: number,
     otherStateToSet?: Partial<StateChangeOptions<Item>>,
