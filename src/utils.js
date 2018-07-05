1b265c237f8247516f923c4f89ec8398317e0cdd
Stian Didriksen <stipsan@gmail.com>
2018-07-05 20:48:17 +0200

    swap implementation

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,4 @@
-import scrollIntoViewPonyfill from 'scroll-into-view-if-needed'
+import computeScrollIntoView from 'compute-scroll-into-view'
 
 let idCounter = 0
 
@@ -25,10 +25,14 @@ function scrollIntoView(node, rootNode) {
     return
   }
 
-  scrollIntoViewPonyfill(node, {
+  const actions = computeScrollIntoView(node, {
     boundary: rootNode,
     block: 'nearest',
     scrollMode: 'if-needed',
+  })
+  actions.forEach(({el, top, left}) => {
+    el.scrollTop = top
+    el.scrollLeft = left
   })
 }
 
