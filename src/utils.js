01b7dad511d73a2aff5c09792ee9b983ae668bb9
Cody Olsen <stipsan@gmail.com>
2018-07-09 20:17:27 +0200

    fix(utils): Replace utils.scrollIntoView with compute-scroll-into-view (#487)
    * Add dependency
    
    * replace utils.scrollIntoView with new lib
    
    * pass a boundary so the behavior is still the same
    
    * better implementation
    
    * update contributors
    
    * replace package with the lower level one
    
    * swap implementation
    
    * bump version
    
    * clientHeight should match clientRect.height
    
    * add offsetheight
    
    * make sure parentNode works properly
    
    * remove tests that conflict with spec
    
    * update contrib
    
    * can't repro locally, try run ci again

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,5 @@
+import computeScrollIntoView from 'compute-scroll-into-view'
+
 let idCounter = 0
 
 /**
@@ -13,98 +15,25 @@ function cbToCb(cb) {
 }
 function noop() {}
 
-function findParent(finder, node, rootNode) {
-  if (node !== null && node !== rootNode.parentNode) {
-    if (finder(node)) {
-      if (node === document.body && node.scrollTop === 0) {
-        // in chrome body.scrollTop always return 0
-        return document.documentElement
-      }
-      return node
-    } else {
-      return findParent(finder, node.parentNode, rootNode)
-    }
-  } else {
-    return null
-  }
-}
-
-/**
- * Get the closest element that scrolls
- * @param {HTMLElement} node - the child element to start searching for scroll parent at
- * @param {HTMLElement} rootNode - the root element of the component
- * @return {HTMLElement} the closest parentNode that scrolls
- */
-const getClosestScrollParent = findParent.bind(
-  null,
-  node => node.scrollHeight > node.clientHeight,
-)
-
 /**
  * Scroll node into view if necessary
  * @param {HTMLElement} node - the element that should scroll into view
  * @param {HTMLElement} rootNode - the root element of the component
- * @param {Boolean} alignToTop - align element to the top of the visible area of the scrollable ancestor
  */
-// eslint-disable-next-line complexity
 function scrollIntoView(node, rootNode) {
-  const scrollParent = getClosestScrollParent(node, rootNode)
-  if (scrollParent === null) {
-    return
-  }
-  const scrollParentStyles = getComputedStyle(scrollParent)
-  const scrollParentRect = scrollParent.getBoundingClientRect()
-  const scrollParentBorderTopWidth = parseInt(
-    scrollParentStyles.borderTopWidth,
-    10,
-  )
-  const scrollParentBorderBottomWidth = parseInt(
-    scrollParentStyles.borderBottomWidth,
-    10,
-  )
-  const bordersWidth =
-    scrollParentBorderTopWidth + scrollParentBorderBottomWidth
-  const scrollParentTop = scrollParentRect.top + scrollParentBorderTopWidth
-  const nodeRect = node.getBoundingClientRect()
-
-  if (nodeRect.top < 0 && scrollParentRect.top < 0) {
-    scrollParent.scrollTop += nodeRect.top
+  if (node === null) {
     return
   }
 
-  if (nodeRect.top < 0) {
-    // the item is above the viewport and the parent is not above the viewport
-    scrollParent.scrollTop += nodeRect.top - scrollParentTop
-    return
-  }
-
-  if (nodeRect.top > 0 && scrollParentRect.top < 0) {
-    if (
-      scrollParentRect.bottom > 0 &&
-      nodeRect.bottom + bordersWidth > scrollParentRect.bottom
-    ) {
-      // the item is below scrollable area
-      scrollParent.scrollTop +=
-        nodeRect.bottom - scrollParentRect.bottom + bordersWidth
-    }
-    // item and parent top are on different sides of view top border (do nothing)
-    return
-  }
-
-  const nodeOffsetTop = nodeRect.top + scrollParent.scrollTop
-  const nodeTop = nodeOffsetTop - scrollParentTop
-  if (nodeTop < scrollParent.scrollTop) {
-    // the item is above the scrollable area
-    scrollParent.scrollTop = nodeTop
-  } else if (
-    nodeTop + nodeRect.height + bordersWidth >
-    scrollParent.scrollTop + scrollParentRect.height
-  ) {
-    // the item is below the scrollable area
-    scrollParent.scrollTop =
-      nodeTop + nodeRect.height - scrollParentRect.height + bordersWidth
-  }
-  // the item is within the scrollable area (do nothing)
+  const actions = computeScrollIntoView(node, {
+    boundary: rootNode,
+    block: 'nearest',
+    scrollMode: 'if-needed',
+  })
+  actions.forEach(({el, top, left}) => {
+    el.scrollTop = top
+    el.scrollLeft = left
+  })
 }
 
 /**
@@ -313,7 +242,6 @@ export {
   callAll,
   debounce,
   scrollIntoView,
-  findParent,
   generateId,
   getA11yStatusMessage,
   unwrapArray,
