328c9ce2cfa0d6c3433ea2d3a56ce61eb494d3fc
Mateusz Burzyński <mateuszburzynski@gmail.com>
2018-08-15 00:19:36 +0200

    chore: wrap stateChangeTypes in NODE_ENV and make them numbers for production

--- src/stateChangeTypes.js
+++ src/stateChangeTypes.js
@@ -1,15 +1,25 @@
-export const unknown = '__autocomplete_unknown__'
-export const mouseUp = '__autocomplete_mouseup__'
-export const itemMouseEnter = '__autocomplete_item_mouseenter__'
-export const keyDownArrowUp = '__autocomplete_keydown_arrow_up__'
-export const keyDownArrowDown = '__autocomplete_keydown_arrow_down__'
-export const keyDownEscape = '__autocomplete_keydown_escape__'
-export const keyDownEnter = '__autocomplete_keydown_enter__'
-export const clickItem = '__autocomplete_click_item__'
-export const blurInput = '__autocomplete_blur_input__'
-export const changeInput = '__autocomplete_change_input__'
-export const keyDownSpaceButton = '__autocomplete_keydown_space_button__'
-export const clickButton = '__autocomplete_click_button__'
-export const blurButton = '__autocomplete_blur_button__'
-export const controlledPropUpdatedSelectedItem = '__autocomplete_controlled_prop_updated_selected_item__'
-export const touchStart = '__autocomplete_touchstart__'
+import productionEnum from './productionEnum.macro'
+
+export const unknown = productionEnum('__autocomplete_unknown__')
+export const mouseUp = productionEnum('__autocomplete_mouseup__')
+export const itemMouseEnter = productionEnum('__autocomplete_item_mouseenter__')
+export const keyDownArrowUp = productionEnum(
+  '__autocomplete_keydown_arrow_up__',
+)
+export const keyDownArrowDown = productionEnum(
+  '__autocomplete_keydown_arrow_down__',
+)
+export const keyDownEscape = productionEnum('__autocomplete_keydown_escape__')
+export const keyDownEnter = productionEnum('__autocomplete_keydown_enter__')
+export const clickItem = productionEnum('__autocomplete_click_item__')
+export const blurInput = productionEnum('__autocomplete_blur_input__')
+export const changeInput = productionEnum('__autocomplete_change_input__')
+export const keyDownSpaceButton = productionEnum(
+  '__autocomplete_keydown_space_button__',
+)
+export const clickButton = productionEnum('__autocomplete_click_button__')
+export const blurButton = productionEnum('__autocomplete_blur_button__')
+export const controlledPropUpdatedSelectedItem = productionEnum(
+  '__autocomplete_controlled_prop_updated_selected_item__',
+)
+export const touchStart = productionEnum('__autocomplete_touchstart__')
