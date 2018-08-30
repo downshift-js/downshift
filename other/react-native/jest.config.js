2700f3e5bce8dc1da42a5aa69d2f66eda2d8c11d
Alexander Nanberg <alexander.nanberg@strateg.se>
2018-08-30 10:51:18 +0200

    fix: react-native tests

--- other/react-native/jest.config.js
+++ other/react-native/jest.config.js
@@ -4,5 +4,8 @@ module.exports = {
   preset: 'react-native',
   rootDir: '../../',
   roots: ['.'],
+  transform: {
+    '^.+\\.js$': '<rootDir>/node_modules/react-native/jest/preprocessor.js',
+  },
   testMatch: ['<rootDir>/other/react-native/__tests__/**/*.js?(x)'],
 }
