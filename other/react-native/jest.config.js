8b26d4a36482feb0872ef3190e8b7ee3462d981c
Alexander Nanberg <alex.nanberg@hotmail.com>
2018-08-30 16:07:21 +0200

    chore: upgrade dependencies and fix react-native build error (#565)
    * chore: update deps and fix build
    
    * chore: upgrade kcd-scripts and babel
    
    * chore: update snapshot
    
    * fix: react-native tests

--- other/react-native/jest.config.js
+++ other/react-native/jest.config.js
@@ -1,10 +1,11 @@
-const jestConfig = require('kcd-scripts/config').jest
+// const jestConfig = require('kcd-scripts/config').jest
 
-module.exports = Object.assign(jestConfig, {
+module.exports = {
   preset: 'react-native',
-  testEnvironment: 'node',
-  transformIgnorePatterns: ['<rootDir>/node_modules/(?!react-native)/'],
   rootDir: '../../',
   roots: ['.'],
+  transform: {
+    '^.+\\.js$': '<rootDir>/node_modules/react-native/jest/preprocessor.js',
+  },
   testMatch: ['<rootDir>/other/react-native/__tests__/**/*.js?(x)'],
-})
+}
