da3863e2b96287bc88914c706b6fdaac614c390a
Mateusz Burzyński <mateuszburzynski@gmail.com>
2018-10-11 20:43:32 +0200

    feat(size): update kcd-scripts (#600)
    **Motivation**
    https://github.com/kentcdodds/kcd-scripts/pull/71

--- other/misc-tests/jest.config.js
+++ other/misc-tests/jest.config.js
@@ -1,6 +1,13 @@
 const jestConfig = require('kcd-scripts/config').jest
+const babelHelpersList = require('@babel/helpers').list
 
 module.exports = Object.assign(jestConfig, {
   roots: ['.'],
   testEnvironment: 'jsdom',
+  moduleNameMapper: babelHelpersList.reduce((aliasMap, helper) => {
+    aliasMap[
+      `@babel/runtime/helpers/esm/${helper}`
+    ] = `@babel/runtime/helpers/${helper}`
+    return aliasMap
+  }, {}),
 })
