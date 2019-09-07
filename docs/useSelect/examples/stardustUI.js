168c25ee8f9a261c06a8cf8f0929ff8f2383b3d5
silviuavram <silviu.avram91@gmail.com>
2019-09-07 12:42:17 +0200

    moved items and styles out of mdx

---
+++ docs/useSelect/examples/stardustUI.js
@@ -0,0 +1,50 @@
+const items = [
+  {
+    header: 'Bruce Wayne',
+    content: 'Software Engineer',
+  },
+  {
+    header: 'Natasha Romanoff',
+    content: 'UX Designer 2',
+  },
+  {
+    header: 'Steven Strange',
+    content: 'Principal Software Engineering Manager',
+  },
+  {
+    header: 'Alfred Pennyworth',
+    content: 'Technology Consultant',
+  },
+  {
+    header: `Scarlett O'Hara`,
+    content: 'Software Engineer 2',
+  },
+  {
+    header: 'Imperator Furiosa',
+    content: 'Boss',
+  },
+  {
+    header: 'Bruce Banner',
+    content: 'Senior Computer Scientist',
+  },
+  {
+    header: 'Peter Parker',
+    content: 'Partner Software Engineer',
+  },
+  {
+    header: 'Selina Kyle',
+    content: 'Graphic Designer',
+  },
+]
+
+const menuStyles = {
+  maxHeight: '250px',
+  overflowY: 'auto',
+  position: 'absolute',
+  margin: 0,
+  borderTop: 0,
+  background: 'white',
+  zIndex: 1000,
+}
+
+export {items, menuStyles}
