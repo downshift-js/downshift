7bb45124340108cc4195ef6742482a8d9c2ce988
Silviu Alexandru Avram <silviu.avram91@gmail.com>
2019-10-27 14:16:53 +0100

    fix(useSelect): don't select item at blur (#796)
    * remove highlight on menu mouse leave
    
    * do not select on blur
    
    * update docs

--- src/hooks/useSelect/index.js
+++ src/hooks/useSelect/index.js
@@ -255,6 +255,11 @@ function useSelect(userProps = {}) {
       })
     }
   }
+  const menuHandleMouseLeave = () => {
+    dispatch({
+      type: stateChangeTypes.MenuMouseLeave,
+    })
+  }
   const toggleButtonHandleClick = () => {
     dispatch({
       type: stateChangeTypes.ToggleButtonClick,
@@ -329,6 +334,7 @@ function useSelect(userProps = {}) {
   const getMenuProps = ({
     onKeyDown,
     onBlur,
+    onMouseLeave,
     refKey = 'ref',
     ref,
     ...rest
@@ -345,6 +351,7 @@ function useSelect(userProps = {}) {
     }),
     onKeyDown: callAllEventHandlers(onKeyDown, menuHandleKeyDown),
     onBlur: callAllEventHandlers(onBlur, menuHandleBlur),
+    onMouseLeave: callAllEventHandlers(onMouseLeave, menuHandleMouseLeave),
     ...rest,
   })
   const getToggleButtonProps = ({
--- src/hooks/useSelect/stateChangeTypes.js
+++ src/hooks/useSelect/stateChangeTypes.js
@@ -10,6 +10,7 @@ export const MenuKeyDownEnd = productionEnum('__menu_keydown_end__')
 export const MenuKeyDownEnter = productionEnum('__menu_keydown_enter__')
 export const MenuKeyDownCharacter = productionEnum('__menu_keydown_character__')
 export const MenuBlur = productionEnum('__menu_blur__')
+export const MenuMouseLeave = productionEnum('__menu_mouse_leave__')
 export const ItemMouseMove = productionEnum('__item_mouse_move__')
 export const ItemClick = productionEnum('__item_click__')
 export const ToggleButtonKeyDownCharacter = productionEnum(
