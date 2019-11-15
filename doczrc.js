5eb8ef509f7265db84ef1a6f0d765281aaacf8e4
Eliot Ostling <eostling@cnm.edu>
2019-11-14 19:29:51 -0500

    Added regex for specific file pattern match

--- doczrc.js
+++ doczrc.js
@@ -1,5 +1,9 @@
-export default {
+
+
+ const files = /(.md|.mdx|.markdown)$/i;
+
+ export default {
   port: 6006,
-  files: './docs/**/*.mdx',
+  files: `./docs/**/${files}`,
   menu: ['Home', {name: 'useSelect', menu: ['Usage', 'UI Libraries']}, 'Tests'],
 }
