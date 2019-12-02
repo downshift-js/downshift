c90e41b9cdce8be4f04fde3b46ae775d2cd915b7
Silviu Alexandru Avram <silviu.avram91@gmail.com>
2019-12-02 10:54:56 +0100

    perf(getItemProps): memoize the event handlers for item (#841)
    * memoize the event handlers for item
    
    * use getItemId instead
    
    * use fastMemoize instead of lodash

--- src/downshift.js
+++ src/downshift.js
@@ -3,6 +3,7 @@
 import PropTypes from 'prop-types'
 import {Component, cloneElement} from 'react'
 import {isForwardRef} from 'react-is'
+import memoize from 'fast-memoize'
 import {isPreact, isReactNative} from './is.macro'
 import setA11yStatus from './set-a11y-status'
 import * as stateChangeTypes from './stateChangeTypes'
@@ -833,10 +834,8 @@ class Downshift extends Component {
         !!this.props.environment.document.activeElement &&
         !!this.props.environment.document.activeElement.dataset &&
         this.props.environment.document.activeElement.dataset.toggle &&
-        (this._rootNode &&
-          this._rootNode.contains(
-            this.props.environment.document.activeElement,
-          ))
+        this._rootNode &&
+        this._rootNode.contains(this.props.environment.document.activeElement)
       if (!this.isMouseDown && !downshiftButtonIsActive) {
         this.reset({type: stateChangeTypes.blurInput})
       }
@@ -869,6 +868,49 @@ class Downshift extends Component {
   }
   //\\\\\\\\\\\\\\\\\\\\\\\\\\\\\ MENU
 
+  getItemPropsEventHandlers = memoize(
+    (index, item, onClick, onPress, onMouseMove, onMouseDown) => {
+      const onSelectKey = isReactNative
+        ? /* istanbul ignore next (react-native) */ 'onPress'
+        : 'onClick'
+      const customClickHandler = isReactNative
+        ? /* istanbul ignore next (react-native) */ onPress
+        : onClick
+
+      return {
+        // onMouseMove is used over onMouseEnter here. onMouseMove
+        // is only triggered on actual mouse movement while onMouseEnter
+        // can fire on DOM changes, interrupting keyboard navigation
+        onMouseMove: callAllEventHandlers(onMouseMove, () => {
+          if (index === this.getState().highlightedIndex) {
+            return
+          }
+          this.setHighlightedIndex(index, {
+            type: stateChangeTypes.itemMouseEnter,
+          })
+
+          // We never want to manually scroll when changing state based
+          // on `onMouseMove` because we will be moving the element out
+          // from under the user which is currently scrolling/moving the
+          // cursor
+          this.avoidScrolling = true
+          this.internalSetTimeout(() => (this.avoidScrolling = false), 250)
+        }),
+        onMouseDown: callAllEventHandlers(onMouseDown, event => {
+          // This prevents the activeElement from being changed
+          // to the item so it can remain with the current activeElement
+          // which is a more common use case.
+          event.preventDefault()
+        }),
+        [onSelectKey]: callAllEventHandlers(customClickHandler, () => {
+          this.selectItemAtIndex(index, {
+            type: stateChangeTypes.clickItem,
+          })
+        }),
+      }
+    },
+  )
+
   /////////////////////////////// ITEM
   getItemProps = ({
     onMouseMove,
@@ -888,44 +930,14 @@ class Downshift extends Component {
       this.items[index] = item
     }
 
-    const onSelectKey = isReactNative
-      ? /* istanbul ignore next (react-native) */ 'onPress'
-      : 'onClick'
-    const customClickHandler = isReactNative
-      ? /* istanbul ignore next (react-native) */ onPress
-      : onClick
-
-    const enabledEventHandlers = {
-      // onMouseMove is used over onMouseEnter here. onMouseMove
-      // is only triggered on actual mouse movement while onMouseEnter
-      // can fire on DOM changes, interrupting keyboard navigation
-      onMouseMove: callAllEventHandlers(onMouseMove, () => {
-        if (index === this.getState().highlightedIndex) {
-          return
-        }
-        this.setHighlightedIndex(index, {
-          type: stateChangeTypes.itemMouseEnter,
-        })
-
-        // We never want to manually scroll when changing state based
-        // on `onMouseMove` because we will be moving the element out
-        // from under the user which is currently scrolling/moving the
-        // cursor
-        this.avoidScrolling = true
-        this.internalSetTimeout(() => (this.avoidScrolling = false), 250)
-      }),
-      onMouseDown: callAllEventHandlers(onMouseDown, event => {
-        // This prevents the activeElement from being changed
-        // to the item so it can remain with the current activeElement
-        // which is a more common use case.
-        event.preventDefault()
-      }),
-      [onSelectKey]: callAllEventHandlers(customClickHandler, () => {
-        this.selectItemAtIndex(index, {
-          type: stateChangeTypes.clickItem,
-        })
-      }),
-    }
+    const enabledEventHandlers = this.getItemPropsEventHandlers(
+      index,
+      item,
+      onClick,
+      onMouseDown,
+      onMouseMove,
+      onPress,
+    )
 
     // Passing down the onMouseDown handler to prevent redirect
     // of the activeElement if clicking on disabled items
