e882d9452e0c34106cb9f9901614758199ddf1ee
Silviu Avram <silviu.avram91@gmail.com>
2020-02-23 13:08:55 +0200

    add propTypes

--- src/hooks/useMultipleSelection/utils.js
+++ src/hooks/useMultipleSelection/utils.js
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types'
 import {
   getInitialValue as getInitialValueCommon,
   defaultProps as defaultPropsCommon,
@@ -19,9 +20,29 @@ function getInitialState(props) {
   }
 }
 
+const propTypes = {
+  items: PropTypes.array.isRequired,
+  onItemRemoved: PropTypes.func,
+  itemToString: PropTypes.func,
+  getA11yRemovalMessage: PropTypes.func,
+  circularNavigation: PropTypes.bool,
+  stateReducer: PropTypes.func,
+  activeIndex: PropTypes.number,
+  onActiveIndexChange: PropTypes.func,
+  environment: PropTypes.shape({
+    addEventListener: PropTypes.func,
+    removeEventListener: PropTypes.func,
+    document: PropTypes.shape({
+      getElementById: PropTypes.func,
+      activeElement: PropTypes.any,
+      body: PropTypes.any,
+    }),
+  }),
+}
+
 const defaultProps = {
   itemToString: defaultPropsCommon.itemToString,
   stateReducer: defaultPropsCommon.stateReducer,
 }
 
-export {getInitialState, defaultProps}
+export {getInitialState, defaultProps, propTypes}
