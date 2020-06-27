9c93ffa7060136492f0cf169eeb113d9fafe1727
Silviu Avram <silviu.avram91@gmail.com>
2020-06-27 15:35:02 +0300

    fix itemToString type

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -88,7 +88,7 @@ export interface A11yStatusMessageOptions<Item> {
   highlightedIndex: number | null
   inputValue: string
   isOpen: boolean
-  itemToString: (item: Item) => string
+  itemToString: (item: Item | null) => string
   previousResultCount: number
   resultCount: number
   highlightedItem: Item
