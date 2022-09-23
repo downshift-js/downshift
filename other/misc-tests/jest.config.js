426c18064b624d8e0ee0b2e0db2608bc6f0e5dbb
Silviu Alexandru Avram <silviu.avram91@gmail.com>
2022-09-23 14:54:33 +0300

    use module mapper

--- other/misc-tests/jest.config.js
+++ other/misc-tests/jest.config.js
@@ -1,13 +1,18 @@
-const jestConfig = require('kcd-scripts/config').jest
+const jestConfig = require('kcd-scripts/jest')
 const babelHelpersList = require('@babel/helpers').list
 
 module.exports = Object.assign(jestConfig, {
   roots: ['.'],
   testEnvironment: 'jsdom',
-  moduleNameMapper: babelHelpersList.reduce((aliasMap, helper) => {
-    aliasMap[
-      `@babel/runtime/helpers/esm/${helper}`
-    ] = `@babel/runtime/helpers/${helper}`
-    return aliasMap
-  }, {}),
+  moduleNameMapper: babelHelpersList.reduce(
+    (aliasMap, helper) => {
+      aliasMap[
+        `@babel/runtime/helpers/esm/${helper}`
+      ] = `@babel/runtime/helpers/${helper}`
+      return aliasMap
+    },
+    {
+      preact: require.resolve('preact'),
+    },
+  ),
 })
