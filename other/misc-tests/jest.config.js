6d6f415e9e03bc7367720cfbfd10ee3b908c3c7e
Silviu Alexandu Avram <silviu.avram91@gmail.com>
2023-09-16 13:03:35 +0300

    fix preact test

--- other/misc-tests/jest.config.js
+++ other/misc-tests/jest.config.js
@@ -4,10 +4,13 @@ const babelHelpersList = require('@babel/helpers').list
 module.exports = Object.assign(jestConfig, {
   roots: ['.'],
   testEnvironment: 'jsdom',
-  moduleNameMapper: babelHelpersList.reduce((aliasMap, helper) => {
-    aliasMap[
-      `@babel/runtime/helpers/esm/${helper}`
-    ] = `@babel/runtime/helpers/${helper}`
-    return aliasMap
-  }, {}),
+  moduleNameMapper: babelHelpersList.reduce(
+    (aliasMap, helper) => {
+      aliasMap[
+        `@babel/runtime/helpers/esm/${helper}`
+      ] = `@babel/runtime/helpers/${helper}`
+      return aliasMap
+    },
+    {'^preact(/(.*)|$)': 'preact$1'},
+  ),
 })
