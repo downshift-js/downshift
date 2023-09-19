d6aeeb9f13e4b0ff0052925d75a741c34810af5b
Silviu Alexandu Avram <silviu.avram91@gmail.com>
2023-09-19 10:31:18 +0300

    update import tests

--- other/misc-tests/__tests__/build.js
+++ other/misc-tests/__tests__/build.js
@@ -11,19 +11,45 @@
  */
 import assert from 'assert'
 
-import esImport from '../../../dist/downshift.esm'
+import esImport, {
+  useCombobox as useComboboxEsImport,
+  useSelect as useSelectEsImport,
+  useMultipleSelection as useMultipleSelectionEsImport,
+} from '../../../dist/downshift.esm'
 
-import cjsImport from '../../../' // picks up the main from package.json
+import cjsImport, {
+  useCombobox as useComboboxCjsImport,
+  useSelect as useSelectCjsImport,
+  useMultipleSelection as useMultipleSelectionCjsImport,
+} from '../../../' // picks up the main from package.json
 
-import umdImport from '../../../dist/downshift.umd'
+import umdImport, {
+  useCombobox as useComboboxUmdImport,
+  useSelect as useSelectUmdImport,
+  useMultipleSelection as useMultipleSelectionUmdImport,
+} from '../../../dist/downshift.umd'
+
+import rnImport, {
+  useCombobox as useComboboxRnImport,
+  useSelect as useSelectRnImport,
+  useMultipleSelection as useMultipleSelectionRnImport,
+} from '../../../dist/downshift.native.cjs'
+
+import rnWebImport, {
+  useCombobox as useComboboxRnWebImport,
+  useSelect as useSelectRnWebImport,
+  useMultipleSelection as useMultipleSelectionRnWebImport,
+} from '../../../dist/downshift.nativeweb.cjs'
 
 // intentionally left out because you shouldn't ever
 // try to require the ES file in CommonJS
 // const esRequire = require('../../../dist/downshift.es')
 const cjsRequire = require('../../../') // picks up the main from package.json
 const umdRequire = require('../../../dist/downshift.umd')
+const rnCjsRequire = require('../../../dist/downshift.native.cjs')
+const rnWebCjsRequire = require('../../../dist/downshift.native.cjs')
 
-test('stuff is good', () => {
+test('downshift component is imported', () => {
   assert(
     isDownshiftComponent(esImport),
     'ES build has a problem with ES Modules',
@@ -49,7 +75,168 @@ test('stuff is good', () => {
     'UMD build has a problem with CJS',
   )
 
+  assert(
+    isDownshiftComponent(rnImport),
+    'React Native build has a problem with ES Modules',
+  )
+
+  assert(
+    isDownshiftComponent(rnCjsRequire.default),
+    'React Native build has a problem with CJS',
+  )
+
+  assert(
+    isDownshiftComponent(rnWebImport),
+    'React Native Web build has a problem with ES Modules',
+  )
+
+  assert(
+    isDownshiftComponent(rnWebCjsRequire.default),
+    'React Native Web build has a problem with CJS',
+  )
+
   // TODO: how could we validate the global export?
+})
+
+test('useSelect hook is imported', () => {
+  assert(
+    isDownshiftComponent(useSelectEsImport),
+    'ES build has a problem with ES Modules',
+  )
+
+  assert(
+    isDownshiftComponent(useSelectCjsImport),
+    'CJS build has a problem with ES Modules',
+  )
+
+  assert(
+    isDownshiftComponent(cjsRequire.useSelect),
+    'CJS build has a problem with CJS',
+  )
+
+  assert(
+    isDownshiftComponent(useSelectUmdImport),
+    'UMD build has a problem with ES Modules',
+  )
+
+  assert(
+    isDownshiftComponent(umdRequire.useSelect),
+    'UMD build has a problem with CJS',
+  )
+
+  assert(
+    isDownshiftComponent(useSelectRnImport),
+    'React Native build has a problem with ES Modules',
+  )
+
+  assert(
+    isDownshiftComponent(rnCjsRequire.useSelect),
+    'React Native build has a problem with CJS',
+  )
+
+  assert(
+    isDownshiftComponent(useSelectRnWebImport),
+    'React Native Web build has a problem with ES Modules',
+  )
+
+  assert(
+    isDownshiftComponent(rnWebCjsRequire.useSelect),
+    'React Native Web build has a problem with CJS',
+  )
+})
+
+test('useCombobox hook is imported', () => {
+  assert(
+    isDownshiftComponent(useComboboxEsImport),
+    'ES build has a problem with ES Modules',
+  )
+
+  assert(
+    isDownshiftComponent(useComboboxCjsImport),
+    'CJS build has a problem with ES Modules',
+  )
+
+  assert(
+    isDownshiftComponent(cjsRequire.useCombobox),
+    'CJS build has a problem with CJS',
+  )
+
+  assert(
+    isDownshiftComponent(useComboboxUmdImport),
+    'UMD build has a problem with ES Modules',
+  )
+
+  assert(
+    isDownshiftComponent(umdRequire.useCombobox),
+    'UMD build has a problem with CJS',
+  )
+
+  assert(
+    isDownshiftComponent(useComboboxRnImport),
+    'React Native build has a problem with ES Modules',
+  )
+
+  assert(
+    isDownshiftComponent(rnCjsRequire.useCombobox),
+    'React Native build has a problem with CJS',
+  )
+
+  assert(
+    isDownshiftComponent(useComboboxRnWebImport),
+    'React Native Web build has a problem with ES Modules',
+  )
+
+  assert(
+    isDownshiftComponent(rnWebCjsRequire.useCombobox),
+    'React Native Web build has a problem with CJS',
+  )
+})
+
+test('useMultipleSelection hook is imported', () => {
+  assert(
+    isDownshiftComponent(useMultipleSelectionEsImport),
+    'ES build has a problem with ES Modules',
+  )
+
+  assert(
+    isDownshiftComponent(useMultipleSelectionCjsImport),
+    'CJS build has a problem with ES Modules',
+  )
+
+  assert(
+    isDownshiftComponent(cjsRequire.useMultipleSelection),
+    'CJS build has a problem with CJS',
+  )
+
+  assert(
+    isDownshiftComponent(useMultipleSelectionUmdImport),
+    'UMD build has a problem with ES Modules',
+  )
+
+  assert(
+    isDownshiftComponent(umdRequire.useMultipleSelection),
+    'UMD build has a problem with CJS',
+  )
+
+  assert(
+    isDownshiftComponent(useMultipleSelectionRnImport),
+    'React Native build has a problem with ES Modules',
+  )
+
+  assert(
+    isDownshiftComponent(rnCjsRequire.useMultipleSelection),
+    'React Native build has a problem with CJS',
+  )
+
+  assert(
+    isDownshiftComponent(useMultipleSelectionRnWebImport),
+    'React Native Web build has a problem with ES Modules',
+  )
+
+  assert(
+    isDownshiftComponent(rnWebCjsRequire.useMultipleSelection),
+    'React Native Web build has a problem with CJS',
+  )
 })
 
 function isDownshiftComponent(thing) {
