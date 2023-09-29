030809f2649e103b631460596f152b1b6852ef59
Silviu Alexandu Avram <silviu.avram91@gmail.com>
2023-09-29 10:24:47 +0300

    try to repro infinite loop

--- docusaurus/pages/useSelect.js
+++ docusaurus/pages/useSelect.js
@@ -3,6 +3,23 @@ import * as React from 'react'
 import {useSelect} from '../../src'
 import {colors, containerStyles, menuStyles} from '../utils'
 
+const relationshipType = [
+  {
+    value: 'Parent',
+    label: 'Parent',
+  },
+  {
+    value: 'Sibling',
+    label: 'Sibling',
+  },
+  {
+    value: 'Child',
+    label: 'Child',
+  },
+]
+
+const itemToString = item => item?.value ?? ''
+
 export default function DropdownSelect() {
   const {
     isOpen,
@@ -12,7 +29,23 @@ export default function DropdownSelect() {
     getMenuProps,
     highlightedIndex,
     getItemProps,
-  } = useSelect({items: colors})
+  } = useSelect({
+    items: relationshipType,
+    itemToString,
+    stateReducer: (state, { changes, type }) => {
+      console.log(state, changes, type);
+      return changes;
+    }
+  })
+
+  const [_reference, _setReference] = React.useState(null)
+
+  const setReference = React.useCallback(
+    node => {
+      _setReference(node)
+    },
+    [_setReference],
+  )
 
   return (
     <div style={containerStyles}>
@@ -21,7 +54,7 @@ export default function DropdownSelect() {
           fontWeight: 'bolder',
           color: selectedItem ? selectedItem : 'black',
         }}
-        {...getLabelProps()}
+        {...getLabelProps({ref: setReference})}
       >
         Choose an element:
       </label>
@@ -35,27 +68,29 @@ export default function DropdownSelect() {
         }}
         {...getToggleButtonProps()}
       >
-        {selectedItem ?? 'Elements'}
+        {selectedItem ? selectedItem.value : 'Elements'}
         {isOpen ? <>&#8593;</> : <>&#8595;</>}
       </div>
       <ul {...getMenuProps()} style={menuStyles}>
-        {isOpen ?
-          colors.map((item, index) => (
-            <li
-              style={{
-                padding: '4px',
-                backgroundColor: highlightedIndex === index ? '#bde4ff' : null,
-              }}
-              key={`${item}${index}`}
-              {...getItemProps({
-                item,
-                index,
-                'data-testid': `downshift-item-${index}`,
-              })}
-            >
-              {item}
-            </li>
-          )) : null}
+        {isOpen
+          ? relationshipType.map((item, index) => (
+              <li
+                style={{
+                  padding: '4px',
+                  backgroundColor:
+                    highlightedIndex === index ? '#bde4ff' : null,
+                }}
+                key={`${item.value}${index}`}
+                {...getItemProps({
+                  item,
+                  index,
+                  'data-testid': `downshift-item-${index}`,
+                })}
+              >
+                {item.value}
+              </li>
+            ))
+          : null}
       </ul>
     </div>
   )
