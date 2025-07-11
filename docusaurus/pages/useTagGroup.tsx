377feb8e29766fed43f97695d083d0ccc38cf4b4
Silviu Alexandu Avram <silviu.avram91@gmail.com>
2025-07-11 10:55:23 +0300

    change some styles in docusaurus

--- docusaurus/pages/useTagGroup.tsx
+++ docusaurus/pages/useTagGroup.tsx
@@ -1,30 +1,48 @@
 import * as React from 'react'
 
 import {useTagGroup} from '../../src'
+import {colors} from '../utils'
 
-import {
-  colors,
-  removeTagStyles,
-  selectedItemsContainerSyles,
-  selectedItemStyles,
-} from '../utils'
+import './useTagGroup.css'
 
 export default function DropdownMultipleCombobox() {
-  const {getTagProps, getTagRemoveProps, getTagGroupProps} = useTagGroup()
+  const initialItems = colors.slice(0, 5)
+  const {
+    getTagProps,
+    getTagRemoveProps,
+    getTagGroupProps,
+    items,
+    activeIndex,
+  } = useTagGroup({initialItems})
+  const itemsToAdd = colors.filter(color => !items.includes(color))
 
   return (
-    <div {...getTagGroupProps()} style={selectedItemsContainerSyles}>
-      {colors.map((color, index) => (
-        <span style={selectedItemStyles} key={color} {...getTagProps({index})}>
-          {color}
+    <div>
+      <div {...getTagGroupProps()} className="tag-group">
+        {items.map((color, index) => (
           <span
-            style={removeTagStyles}
-            {...getTagRemoveProps({index, 'aria-label': 'Remove color.'})}
+            className={`${index === activeIndex ? 'selected-tag' : ''} tag`}
+            key={color}
+            {...getTagProps({index})}
           >
-            &#10005;
+            {color}
+            <span
+              className="tag-remove-button"
+              {...getTagRemoveProps({index, 'aria-label': 'remove color'})}
+            >
+              &#10005;
+            </span>
           </span>
-        </span>
-      ))}
+        ))}
+      </div>
+      <div>Add more items:</div>
+      <ul>
+        {itemsToAdd.map(item => (
+          <li className="item-to-add" tabIndex={0} key={item}>
+            {item}
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
