2173474175fb9536964b8a4862f6861fa6b5702e
Silviu Alexandru Avram <silviu.avram91@gmail.com>
2026-05-09 14:59:35 +0300

    bring back the previous typings folder

---
+++ typings/index.d.ts
@@ -0,0 +1,15 @@
+// Downshift (legacy class component)
+export * from '../src/downshift.types'
+export {default} from '../src/downshift.types'
+
+// useSelect
+export * from '../src/hooks/useSelect/index.types'
+
+// useCombobox
+export * from '../src/hooks/useCombobox/index.types'
+
+// useMultipleSelection
+export * from '../src/hooks/useMultipleSelection/index.types'
+
+// useTagGroup
+export * from '../src/hooks/useTagGroup/index.types'
