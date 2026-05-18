e1505c6b88eaa2202067c281940e52c9414170da
Silviu Alexandru Avram <silviu.avram91@gmail.com>
2026-05-18 10:08:23 +0300

    fix(docusaurus): update and fix webpack issue

--- docusaurus/plugins/webpack5polyfills.js
+++ docusaurus/plugins/webpack5polyfills.js
@@ -12,6 +12,17 @@ module.exports = function () {
             module: false,
           },
         },
+        module: {
+          rules: [
+            // Some dependencies ship ESM that does directory-style imports
+            // (e.g. yaml's browser dist importing 'buffer'). Webpack 5 strict
+            // ESM resolution requires fully specified paths, so relax it.
+            {
+              test: /\.m?js$/,
+              resolve: {fullySpecified: false},
+            },
+          ],
+        },
         plugins: [new NodePolyfillPlugin()],
       }
     },
